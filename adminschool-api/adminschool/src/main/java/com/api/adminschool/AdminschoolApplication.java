6538f1e7a3425178fd24849417e01523fa658bbc
Natan Bastos <87812110+natanmorais23@users.noreply.github.com>
2023-11-17 10:32:49 -0300

    First Commit

---
+++ adminschool-api/adminschool/src/main/java/com/api/adminschool/AdminschoolApplication.java
@@ -0,0 +1,12 @@
+package com.api.adminschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminschoolApplication.class, args);
+	}
+}
